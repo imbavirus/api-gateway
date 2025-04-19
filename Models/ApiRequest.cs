2f957ccaac55ada13877482dbaeed03258ea9403
Justin van der Westhuizen <justin@giftsinc.co.za>
2025-04-19 04:13:54 +0200

    renamed field to endpoint

--- Models/ApiRequest.cs
+++ Models/ApiRequest.cs
@@ -3,7 +3,7 @@ namespace ApiGateway.Models
     public class ApiRequest
     {
         public int StatusCode { get; set; }
-        public string Request { get; set; }
+        public string Endpoint { get; set; }
         public DateTime Time { get; set; }
     }
 }
